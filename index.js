0e33e6d5554cec4f52a57307d80c65103a945bd2
Marius Du Plooy <woestyn101@gmail.com>
2024-01-22 02:43:22 -0500

    class inheritance fixed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import inquirer from 'inquirer';
 import * as fs from'fs';
 import {Myshape} from './lib/shape.js';
 import {Circle} from './lib/circle.js';
+import {Triangle} from './lib/triangle.js';
+import {Square} from './lib/square.js';
 
 
 inquirer
@@ -35,14 +37,24 @@ inquirer
         message: "What shape color do you prefer?",
         default: ""
     },
+    {
+      type: 'input',
+      name: 'fileName',
+      message: "Type a filename:",
+      default: ""
+  },
   ]) 
   .then((answers) => {
     // Use user feedback for... whatever!!
-     var thecircle = new Circle(answers.text, answers.textColor, answers.shapeColor);
-     
-     fs.writeFile('./examples/main.svg', 
-     thecircle.start + thecircle.circleTag + thecircle.theText + thecircle.end    
-     
+    
+    if (answers.shapeChoice === "circle"){
+      var thecircle = new Circle(answers.text, answers.textColor, answers.shapeColor);
+
+      console.log(thecircle.render());
+
+
+     fs.writeFile('./examples/'+ answers.fileName +'.svg', 
+     thecircle.start + thecircle.render() + thecircle.theText + thecircle.end        
      
      , err => {
       if (err) {
@@ -50,13 +62,52 @@ inquirer
       } else {
         // file written successfully
       }
+
     });
+      
+    } else if(answers.shapeChoice === "triangle") {
+      var thetriangle = new Triangle(answers.text, answers.textColor, answers.shapeColor);
+
+
+     fs.writeFile('./examples/'+ answers.fileName +'.svg', 
+     thetriangle.start + thetriangle.triangleTag + thetriangle.theText + thetriangle.end     
+     , err => {
+      if (err) {
+        console.error(err);
+      } else {
+        // file written successfully
+      }
+
+    });
+
+    } else {
+      var thesquare = new Square(answers.text, answers.textColor, answers.shapeColor);
+
+
+     fs.writeFile('./examples/'+ answers.fileName +'.svg', 
+     thesquare.start + thesquare.squareTag  + thesquare.theText + thesquare.end        
+     
+     , err => {
+      if (err) {
+        console.error(err);
+      } else {
+        // file written successfully
+      }
+
+    });
+    
+    }
+
+
+    
+     
+     
     console.log(answers.text);
     console.log(answers.textColor);
     console.log(answers.shapeChoice);
     console.log(answers.shapeColor);
-    console.log(thecircle.start);
-    console.log(thecircle.end);
+    console.log(thesquare.start);
+    console.log(thesquare.end);
   })
   .catch((error) => {
     if (error.isTtyError) {
